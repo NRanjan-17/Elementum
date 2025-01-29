e0466a5bb4d387ad009d21560aad393cb01ab1ac
NRanjan-17 <nalinishranjan05@gmail.com>
2025-01-29 23:53:24 +0530

    Initial Push

---
+++ Elementum/ElementumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElementumApp.swift
+//  Elementum
+//
+//  Created by Test on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ElementumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ElementumTests/ElementumTests.swift
@@ -0,0 +1,17 @@
+//
+//  ElementumTests.swift
+//  ElementumTests
+//
+//  Created by Test on 29/01/25.
+//
+
+import Testing
+@testable import Elementum
+
+struct ElementumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
